632033862d3989e4bd68c6d1e282a28168619956
Wali Rahman <wali.rahman89@yahoo.com>
2018-09-02 20:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fuzzyMatch (list, partialName) {
   let lengthOfName = partialName.length;
   return list.filter(function (drivername) {
-    return driverName.slice(0, lengthOfName) === partialName
+    return driverName.slice(0, lengthOfName) === partialName;
   });
 }
