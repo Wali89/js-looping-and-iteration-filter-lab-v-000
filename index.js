0bd60dec50f48a0845384472c9515e6313b9bf60
Wali Rahman <wali.rahman89@yahoo.com>
2018-09-02 20:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function fuzzyMatch (list, name) {
   let lengthOfName = partialName.length;
   return list.filter(function (drivername){
     return driverName.slice(0, lengthOfName) === partialName
-  })
+  }):
 }
